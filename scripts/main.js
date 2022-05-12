b350ab056799d32cb12d136e79524dc6acfe3bd8
john cobain <andreygdsn@gmail.com>
2022-05-11 22:48:36 -0300

    screen function

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+const button = document.querySelectorAll('button');
+const screen = document.querySelector('.screen');
+
+function addOnScreen(num){
+  if(num === 'percent'){
+    screen.textContent += '%';
+    return;
+  }
+  if(num === '/'){
+    screen.textContent += '÷';
+    return;
+  }
+  screen.textContent += num;
+}
+
+function removeFromScreen(how){
+  if(how === 'clear'){
+    screen.textContent = '';
+    return;
+  }
+  const text = screen.textContent;
+  const newText = text.slice(0, text.length-1);
+  screen.textContent = newText;
+}
+
+function showResult(){
+  screen.textContent = "I'm the result"
+}
+
+
+button.forEach(btn => btn.addEventListener('click' ,opt => {
+  const option = opt.target.id;
+  if(option === '='){
+    showResult();
+    return;
+  }
+  if(option === 'delete' || option === 'clear'){
+    removeFromScreen(option);
+    return;
+  }
+  addOnScreen(option);
+}));
